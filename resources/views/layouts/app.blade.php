550fc533017daee921d3f66149bb59cb00abd41b
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-27 00:31:49 +0900

    windows

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,5 @@
+
+{{--
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -30,6 +32,31 @@
     </script>
 
 </head>
+--}}
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content=" ～web技術を身近に～  千葉県旭市にてシステム開発からアプリ操作のインストラクションまで、皆さまがより身近にインターネットサービスを利用できるよう、全力でサポートいたします。スマートデバイス操作のインストラクション等もお気軽にご相談ください。私はIT技術の普及に貢献します。">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <link href="https://lara-assist.jp/index.css" rel="stylesheet">
+  <title>ララアシスト ～Web技術を身近に～ 千葉県旭市 システム開発 プログラミングスクール ララアシスト</title>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-YGNQ69DFKK"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-YGNQ69DFKK');
+  </script>
+</head>
+
+{{--
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -82,6 +109,25 @@
                 </div>
             </div>
         </nav>
+--}}
+
+
+<body>
+	<div style="vertical-align: middle; height: 70px; background-color:rgb(220, 220, 210);">
+	  <h1 style="padding-top: 12px; text-align: center; background-color:rgb(220, 220, 210)">ララアシスト</h1>
+	</div>
+
+	<nav class="navbar navbar-expand-md navbar-light bg-thin_brown shadow-sm">
+	    <div class="container">
+	      <a href="https://lara-assist.jp">Top</a>
+	      <a href="{{ url('about') }}">ご挨拶</a>
+	      <a href="{{ url('systems') }}">システム</a>
+	      <a href="{{ url('reservation') }}">リモート予約（調整中）</a>
+	      <a href="{{ url('memo') }}">技術情報</a>
+	      <a href="{{ url('inquiry') }}">お問合せ（調整中）</a>
+	      <a href="{{ url('references') }}">参考サイト</a>
+	    </div>
+	</nav>
 
         <main class="py-4">
             @yield('content')
--- resources/views/sys/index.blade.php
+++ resources/views/sys/index.blade.php
@@ -11,26 +11,6 @@
 
 @include('common.errors')
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content=" ～web技術を身近に～  千葉県旭市にてシステム開発からアプリ操作のインストラクションまで、皆さまがより身近にインターネットサービスを利用できるよう、全力でサポートいたします。スマートデバイス操作のインストラクション等もお気軽にご相談ください。私はIT技術の普及に貢献します。">
-  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-  <link href="https://lara-assist.jp/index.css" rel="stylesheet">
-  <title>ララアシスト ～Web技術を身近に～ 千葉県旭市 システム開発 プログラミングスクール ララアシスト</title>
-
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-YGNQ69DFKK"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-YGNQ69DFKK');
-  </script>
-</head>
 
 <body>
 	<div style="vertical-align: middle; height: 70px; background-color:rgb(220, 220, 210);">
