ecfaefcbe2284e0851119f333eba076016737e81
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-28 02:04:59 +0900

    windows

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'パスワードが再設定されました。',
+    'sent' => 'パスワード再設定用メールを送信しました。',
+    'throttled' => '再送信する前にお待ちください',
+    'token' => '無効な処理です。あらためて再設定を行ってください。',
+    'user' => "メールアドレスが登録されていません。",
+
+];
