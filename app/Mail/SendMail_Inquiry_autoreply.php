6526b683ac8bf8c92e84ae2c64abe50a20eb6450
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-28 22:28:20 +0900

    windows

--- app/Mail/SendMail_Inquiry_autoreply.php
+++ app/Mail/SendMail_Inquiry_autoreply.php
@@ -29,7 +29,7 @@ class SendMail_Inquiry_autoreply extends Mailable
     {
         //textをview、viewをtextに変える事で、html・テキストメールの設定を変えられる
         return $this->text('emails.text_inquiry_autoreply')                 // 使用するviewsテンプレート
-                        ->from('no-reply@lara-assist.jp', 'Test')           // 送信者情報
+                        ->from('no-reply@lara-assist.jp', 'ララアシスト')           // 送信者情報
                         ->subject('お問合せ')                                // 件名
                         ->with(['content'   =>  $this->content]);           // メール本文
     }
