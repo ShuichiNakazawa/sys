bc63a09c418677b484661e765a9539c2d494c912
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-29 03:16:27 +0900

    windows

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(ReservationsTableSeeder::class);
         $this->call(FieldsTableSeeder::class);
-        //$this->call(UsersTableSeeder::class);
+        $this->call(Subject_namesTableSeeder::class);
 
     }
 }
