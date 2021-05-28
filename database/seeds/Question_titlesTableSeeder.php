85cd90480fabdc1c6585550cbdd6426d2133936a
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-29 04:59:11 +0900

    windows

--- database/seeds/Question_titlesTableSeeder.php
+++ database/seeds/Question_titlesTableSeeder.php
@@ -12,5 +12,549 @@ class Question_titlesTableSeeder extends Seeder
     public function run()
     {
         //
+        \DB::table('question_titles')->delete();
+        
+        \DB::table('question_titles')->insert(array (
+            0 => 
+            array (
+                 
+                'subject_name_id' => '13',
+                'title_id'      =>      '0',
+                'questions_title' => '平成３０年度 春期',
+                'sight_key' => 'origin',
+            ),
+            1 => 
+            array (
+                 
+                'subject_name_id' => '13',
+                'title_id'      =>      '0',
+                'questions_title' => '平成３０年度 秋期',
+                'sight_key' => 'origin',
+            ),
+            2 => 
+            array (
+                 
+                'subject_name_id' => '13',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２９年度 秋期',
+                'sight_key' => 'origin',
+            ),
+            3 => 
+            array (
+                 
+                'subject_name_id' => '13',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２９年度 春期',
+                'sight_key' => 'origin',
+            ),
+            4 => 
+            array (
+                 
+                'subject_name_id' => '13',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２８年度 春期',
+                'sight_key' => 'origin',
+            ),
+            5 => 
+            array (
+                 
+                'subject_name_id' => '13',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２８年度 秋期',
+                'sight_key' => 'origin',
+            ),
+            6 => 
+            array (
+                 
+                'subject_name_id' => '13',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２７年度 秋期',
+                'sight_key' => 'origin',
+            ),
+            7 => 
+            array (
+                 
+                'subject_name_id' => '13',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２７年度 春期',
+                'sight_key' => 'origin',
+            ),
+            8 => 
+            array (
+                 
+                'subject_name_id' => '1',
+                'title_id'      =>      '0',
+                'questions_title' => '平成３０年',
+                'sight_key' => 'sample',
+            ),
+            9 => 
+            array (
+                 
+                'subject_name_id' => '1',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２９年',
+                'sight_key' => 'sample',
+            ),
+            10 => 
+            array (
+                 
+                'subject_name_id' => '1',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２８年',
+                'sight_key' => 'sample',
+            ),
+            11 => 
+            array (
+                 
+                'subject_name_id' => '1',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２７年',
+                'sight_key' => 'sample',
+            ),
+            12 => 
+            array (
+                 
+                'subject_name_id' => '1',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２６年',
+                'sight_key' => 'sample',
+            ),
+            13 => 
+            array (
+                 
+                'subject_name_id' => '1',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２５年',
+                'sight_key' => 'sample',
+            ),
+            14 => 
+            array (
+                 
+                'subject_name_id' => '1',
+                'title_id'      =>      '0',
+                'questions_title' => '令和元年',
+                'sight_key' => 'sample',
+            ),
+            15 => 
+            array (
+                 
+                'subject_name_id' => '1',
+                'title_id'      =>      '0',
+                'questions_title' => '令和２年',
+                'sight_key' => 'sample',
+            ),
+            16 => 
+            array (
+                 
+                'subject_name_id' => '11',
+                'title_id'      =>      '44',
+                'questions_title' => '平成３１年春期 午前',
+                'sight_key' => 'origin',
+            ),
+            17 => 
+            array (
+                 
+                'subject_name_id' => '11',
+                'title_id'      =>      '40',
+                'questions_title' => '平成３０年春期 午前',
+                'sight_key' => 'origin',
+            ),
+            18 => 
+            array (
+                 
+                'subject_name_id' => '11',
+                'title_id'      =>      '42',
+                'questions_title' => '平成３０年秋期 午前',
+                'sight_key' => 'origin',
+            ),
+            19 => 
+            array (
+                 
+                'subject_name_id' => '11',
+                'title_id'      =>      '38',
+                'questions_title' => '平成２９年秋期 午前',
+                'sight_key' => 'origin',
+            ),
+            20 => 
+            array (
+                 
+                'subject_name_id' => '11',
+                'title_id'      =>      '36',
+                'questions_title' => '平成２９年春期 午前',
+                'sight_key' => 'origin',
+            ),
+            21 => 
+            array (
+                 
+                'subject_name_id' => '11',
+                'title_id'      =>      '32',
+                'questions_title' => '平成２８年春期 午前',
+                'sight_key' => 'origin',
+            ),
+            22 => 
+            array (
+      
+                'subject_name_id' => '11',
+                'title_id'      =>      '34',
+                'questions_title' => '平成２８年秋期 午前',
+                'sight_key' => 'origin',
+            ),
+            23 => 
+            array (
+         
+                'subject_name_id' => '22',
+                'title_id'      =>      '0',
+                'questions_title' => '令和元年 秋期',
+                'sight_key' => 'origin',
+            ),
+            24 => 
+            array (
+           
+                'subject_name_id' => '22',
+                'title_id'      =>      '0',
+                'questions_title' => '平成３１年 春期',
+                'sight_key' => 'origin',
+            ),
+            25 => 
+            array (
+        
+                'subject_name_id' => '22',
+                'title_id'      =>      '0',
+                'questions_title' => '平成３０年 秋期',
+                'sight_key' => 'origin',
+            ),
+            26 => 
+            array (
+           
+                'subject_name_id' => '22',
+                'title_id'      =>      '0',
+                'questions_title' => '平成３０年 春期',
+                'sight_key' => 'origin',
+            ),
+            27 => 
+            array (
+         
+                'subject_name_id' => '22',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２９年 秋期',
+                'sight_key' => 'origin',
+            ),
+            28 => 
+            array (
+       
+                'subject_name_id' => '22',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２９年 春期',
+                'sight_key' => 'origin',
+            ),
+            29 => 
+            array (
+       
+                'subject_name_id' => '22',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２８年 秋期',
+                'sight_key' => 'origin',
+            ),
+            30 => 
+            array (
+          
+                'subject_name_id' => '22',
+                'title_id'      =>      '0',
+                'questions_title' => '平成２８年 春期',
+                'sight_key' => 'origin',
+            ),
+            31 => 
+            array (
+        
+                'subject_name_id' => '1',
+                'title_id'      =>      '2',
+                'questions_title' => '平成２０年 午前',
+                'sight_key' => 'origin',
+            ),
+            32 => 
+            array (
+        
+                'subject_name_id' => '1',
+                'title_id'      =>      '1',
+                'questions_title' => '平成２０年 午後',
+                'sight_key' => 'origin',
+            ),
+            33 => 
+            array (
+         
+                'subject_name_id' => '2',
+                'title_id'      =>      '2',
+                'questions_title' => '平成２０年 午前',
+                'sight_key' => 'origin',
+            ),
+            34 => 
+            array (
+       
+                'subject_name_id' => '2',
+                'title_id'      =>      '1',
+                'questions_title' => '平成２０年 午後',
+                'sight_key' => 'origin',
+            ),
+            35 => 
+            array (
+        
+                'subject_name_id' => '1',
+                'title_id'      =>      '4',
+                'questions_title' => '平成２１年 午前',
+                'sight_key' => 'origin',
+            ),
+            36 => 
+            array (
+        
+                'subject_name_id' => '1',
+                'title_id'      =>      '3',
+                'questions_title' => '平成２１年 午後',
+                'sight_key' => 'origin',
+            ),
+            37 => 
+            array (
+        
+                'subject_name_id' => '1',
+                'title_id'      =>      '26',
+                'questions_title' => '令和２年 午前',
+                'sight_key' => 'origin',
+            ),
+            38 => 
+            array (
+         
+                'subject_name_id' => '1',
+                'title_id'      =>      '25',
+                'questions_title' => '令和２年 午後',
+                'sight_key' => 'origin',
+            ),
+            39 => 
+            array (
+          
+                'subject_name_id' => '1',
+                'title_id'      =>      '24',
+                'questions_title' => '平成３１年 午前',
+                'sight_key' => 'origin',
+            ),
+            40 => 
+            array (
+           
+                'subject_name_id' => '1',
+                'title_id'      =>      '23',
+                'questions_title' => '平成３１年 午後',
+                'sight_key' => 'origin',
+            ),
+            41 => 
+            array (
+           
+                'subject_name_id' => '1',
+                'title_id'      =>      '22',
+                'questions_title' => '平成３０年 午前',
+                'sight_key' => 'origin',
+            ),
+            42 => 
+            array (
+         
+                'subject_name_id' => '1',
+                'title_id'      =>      '21',
+                'questions_title' => '平成３０年 午後',
+                'sight_key' => 'origin',
+            ),
+            43 => 
+            array (
+             
+                'subject_name_id' => '1',
+                'title_id'      =>      '20',
+                'questions_title' => '平成２９年 午前',
+                'sight_key' => 'origin',
+            ),
+            44 => 
+            array (
+          
+                'subject_name_id' => '1',
+                'title_id'      =>      '19',
+                'questions_title' => '平成２９年 午後',
+                'sight_key' => 'origin',
+            ),
+            45 => 
+            array (
+         
+                'subject_name_id' => '1',
+                'title_id'      =>      '18',
+                'questions_title' => '平成２８年 午前',
+                'sight_key' => 'origin',
+            ),
+            46 => 
+            array (
+           
+                'subject_name_id' => '1',
+                'title_id'      =>      '17',
+                'questions_title' => '平成２８年 午後',
+                'sight_key' => 'origin',
+            ),
+            47 => 
+            array (
+            
+                'subject_name_id' => '1',
+                'title_id'      =>      '16',
+                'questions_title' => '平成２７年 午前',
+                'sight_key' => 'origin',
+            ),
+            48 => 
+            array (
+            
+                'subject_name_id' => '1',
+                'title_id'      =>      '15',
+                'questions_title' => '平成２７年 午後',
+                'sight_key' => 'origin',
+            ),
+            49 => 
+            array (
+             
+                'subject_name_id' => '1',
+                'title_id'      =>      '14',
+                'questions_title' => '平成２６年 午前',
+                'sight_key' => 'origin',
+            ),
+            50 => 
+            array (
+          
+                'subject_name_id' => '1',
+                'title_id'      =>      '13',
+                'questions_title' => '平成２６年 午後',
+                'sight_key' => 'origin',
+            ),
+            51 => 
+            array (
+         
+                'subject_name_id' => '1',
+                'title_id'      =>      '12',
+                'questions_title' => '平成２５年 午前',
+                'sight_key' => 'origin',
+            ),
+            52 => 
+            array (
+               
+                'subject_name_id' => '1',
+                'title_id'      =>      '11',
+                'questions_title' => '平成２５年 午後',
+                'sight_key' => 'origin',
+            ),
+            53 => 
+            array (
+          
+                'subject_name_id' => '1',
+                'title_id'      =>      '10',
+                'questions_title' => '平成２４年 午前',
+                'sight_key' => 'origin',
+            ),
+            54 => 
+            array (
+             
+                'subject_name_id' => '1',
+                'title_id'      =>      '9',
+                'questions_title' => '平成２４年 午後',
+                'sight_key' => 'origin',
+            ),
+            55 => 
+            array (
+           
+                'subject_name_id' => '1',
+                'title_id'      =>      '8',
+                'questions_title' => '平成２３年 午前',
+                'sight_key' => 'origin',
+            ),
+            56 => 
+            array (
+             
+                'subject_name_id' => '1',
+                'title_id'      =>      '7',
+                'questions_title' => '平成２３年 午後',
+                'sight_key' => 'origin',
+            ),
+            57 => 
+            array (
+                'subject_name_id' => '1',
+                'title_id'      =>      '6',
+                'questions_title' => '平成２２年 午前',
+                'sight_key' => 'origin',
+            ),
+            58 => 
+            array (
+
+                'subject_name_id' => '1',
+                'title_id'      =>      '5',
+                'questions_title' => '平成２２年 午後',
+                'sight_key' => 'origin',
+            ),
+            59 => 
+            array (
+                'subject_name_id' => '4',
+                'title_id'      =>      '26',
+                'questions_title' => '令和２年 午前',
+                'sight_key' => 'origin',
+            ),
+            60 => 
+            array (
+                'subject_name_id' => '4',
+                'title_id'      =>      '25',
+                'questions_title' => '令和２年 午後',
+                'sight_key' => 'origin',
+            ),
+            61 => 
+            array (
+                'subject_name_id' => '4',
+                'title_id'      =>      '24',
+                'questions_title' => '平成３１年 午前',
+                'sight_key' => 'origin',
+            ),
+            62 => 
+            array (
+                'subject_name_id' => '4',
+                'title_id'      =>      '23',
+                'questions_title' => '平成３１年 午後',
+                'sight_key' => 'origin',
+            ),
+            63 => 
+            array (
+                'subject_name_id' => '4',
+		'title_id'	=>	'22',
+                'questions_title' => '平成３０年 午前',
+                'sight_key' => 'origin',
+            ),
+            64 => 
+            array (
+                'subject_name_id' => '4',
+		'title_id'	=>	'21',
+                'questions_title' => '平成３０年 午後',
+                'sight_key' => 'origin',
+            ),
+            65 => 
+            array (
+                 
+                'subject_name_id' => '11',
+                'title_id'      =>      '46',
+                'questions_title' => '令和元年秋期 午前',
+                'sight_key' => 'origin',
+            ),
+
+
+            66 => 
+            array (
+                 
+                'subject_name_id' => '2',
+                'title_id'      =>      '26',
+                'questions_title' => '令和２年 午前',
+                'sight_key' => 'origin',
+            ),
+            67 => 
+            array (
+                 
+                'subject_name_id' => '2',
+                'title_id'      =>      '25',
+                'questions_title' => '令和２年 午後',
+                'sight_key' => 'origin',
+            ),
+
+        ));
     }
 }
