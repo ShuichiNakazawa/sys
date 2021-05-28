feab924c637590529a53f89805ee13d030ec2d69
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-29 04:57:32 +0900

    xserver

---
+++ database/seeds/Question_titlesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Question_titlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
