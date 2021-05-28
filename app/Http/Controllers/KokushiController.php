e5320267e516558e8b919779e56224c4bde9b075
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-29 08:44:02 +0900

    windows

--- app/Http/Controllers/KokushiController.php
+++ app/Http/Controllers/KokushiController.php
@@ -65,7 +65,8 @@ class KokushiController extends Controller
         $logind = 0;
       }
 
-      return view('kokushi.menu', (['kokushi', $subject_id]))
+      return view('kokushi.menu')
+      //return view('kokushi.menu', (['kokushi', $subject_id]))
               ->with([
                 'subject_name'    =>    $subject_name_kanji,
                 'titles'          =>    $titles,
