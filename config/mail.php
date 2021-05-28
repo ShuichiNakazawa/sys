42306b9ce2b764413fe5710434bd1e01ffea5aa5
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-28 19:28:17 +0900

    windows

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@lara-assist.jp'),
+        'name' => env('MAIL_FROM_NAME', 'ララアシスト（送信専用）'),
     ],
 
     /*
