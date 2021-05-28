b4a662c28cb21f13c2fad81795dd5e9d64d92180
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-29 03:05:40 +0900

    windows

--- database/seeds/FieldsTableSeeder.php
+++ database/seeds/FieldsTableSeeder.php
@@ -12,5 +12,25 @@ class FieldsTableSeeder extends Seeder
     public function run()
     {
         //
+        \DB::table('fields')->delete();
+        
+        \DB::table('fields')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'field_name' => '医療',
+                'sight_key' => 'origin',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'field_name' => '情報処理',
+                'sight_key' => 'origin',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+        ));
     }
 }
