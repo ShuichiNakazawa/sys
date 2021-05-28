0faa49b1efe24c6202639a57f3becc2d7cb0f40e
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-28 21:55:52 +0900

    windows

--- app/Http/Controllers/MailSendController.php
+++ app/Http/Controllers/MailSendController.php
@@ -23,6 +23,44 @@ class MailSendController extends Controller
             ]
         ];
 
+        // きっかけ エンコード
+        switch($request->trigger){
+            case 0 :
+                $trigger    =   "インターネット検索";
+                break;
+            case 1 :
+                $trigger    =   "ご紹介";
+                break;
+            case 2 :
+                $trrigger   =   "チラシ・広告";
+                break;
+            case 3 :
+                $trrigger   =   "クラウドソーシング";
+                break;
+            case 4 :
+                $trrigger   =   "SNS";
+                break;
+            case 5 :
+                $trrigger   =   "その他";
+                break;
+        }
+
+        // 問い合わせ種類 エンコード
+        switch($request->inquiryType){
+            case 0 :
+                $inquiryType    =   "制作のご依頼";
+                break;
+            case 1 :
+                $inquiryType    =   "制作の見積り";
+                break;
+            case 2 :
+                $inquiryType    =   "Webスクールへの質問";
+                break;
+            case 3 :
+                $inquiryType    =   "その他";
+                break;
+        }
+
         $content    =   
             '会社名・屋号：　'    . $request->companyName . "\n"
             . '顧客名：　'         . $request->customerName . "\n"
@@ -30,8 +68,8 @@ class MailSendController extends Controller
             . '電話番号：　'       . $request->tel . "\n"
             . 'メールアドレス：　'   . $request->mailAddress . "\n"
             . 'webサイトURL：　'    . $request->sightUrl . "\n"
-            . 'きっかけ：　'        . $request->trigger . "\n"
-            . '問合せ種類：　'      . $request->inquiryType . "\n"
+            . 'きっかけ：　'        . $trigger . "\n"
+            . '問合せ種類：　'      . $inquiryType . "\n"
             . '問合せ内容：　'      . $request->inquiryContent . "\n";
 
         // コントローラ使用
