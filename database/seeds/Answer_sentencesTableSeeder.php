67a9505546f4c6c175eb9b0753d5da8b51ac74b1
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-29 07:22:24 +0900

    xserver

---
+++ database/seeds/Answer_sentencesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Answer_sentencesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
---
+++ database/seeds/Choice_sentencesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Choice_sentencesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
