a3070562771bb82bc9e3fd2eaf4187d29ace19f3
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-28 19:03:03 +0900

    windows

--- app/Http/Controllers/MailSendController.php
+++ app/Http/Controllers/MailSendController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mail;
 
 class MailSendController extends Controller
 {
     //
+    public function send(){
+
+        $data = [];
+
+        Mail::send('emails.test', $data, function($message){
+            $message->to('contact@lara-assist.jp', 'Test')
+                        ->subject('This is a test mail');
+        });
+    }
 }
