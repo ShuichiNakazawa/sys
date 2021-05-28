3a0e15ef262493b762895c9bd72dcbe7d2d5b4e5
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-28 20:30:50 +0900

    windows

--- app/Http/Controllers/MailSendController.php
+++ app/Http/Controllers/MailSendController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 //use Mail;
-use App\Http\Mail\SendMail_Inquiry;
+use App\Mail\SendMail_Inquiry;
 
 
 class MailSendController extends Controller
