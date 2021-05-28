9e1255a2ec6649f5a3e93d8476c7b60f85eb8bf0
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-29 04:13:41 +0900

    windows

--- resources/views/kokushi/index.blade.php
+++ resources/views/kokushi/index.blade.php
@@ -30,8 +30,8 @@
         --}}
         @foreach($subjects[$count] as $subject)
           <div class="btn_subject">
-            <a href="{{  url('/' . $subject->id) }}">
-              <button>{{ $subject->subject_name }}</button>
+            <a href="{{  url('/' . $subject[0]) }}">
+              <button>{{ $subject[1] }}</button>
             </a>
           </div>
         @endforeach
