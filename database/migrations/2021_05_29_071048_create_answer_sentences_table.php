7e232bd351238f3ed83ba04d8508d42020793214
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-29 09:17:46 +0900

    windows

--- database/migrations/2021_05_29_071048_create_answer_sentences_table.php
+++ database/migrations/2021_05_29_071048_create_answer_sentences_table.php
@@ -17,8 +17,8 @@ class CreateAnswerSentencesTable extends Migration
             $table->bigIncrements('id');
             $table->integer('subject_name_id')->nullable()->default(99);    // 科目名ID
             $table->string('subject_name');                                 // 科目名（暫定追加）
-            $table->integer('questions_title_id')->default(99);             // 問題集タイトルID
-            $table->string('questions_title');                              // 問題タイトル（暫定追加）
+            $table->integer('question_title_id')->default(99);              // 問題集タイトルID
+            $table->string('question_title');                               // 問題タイトル（暫定追加）
             $table->integer('question_number');                             // 問題番号
             $table->integer('answer_id');                                   // 正解ID
             $table->string('answer_sentence');                              // 正解文
