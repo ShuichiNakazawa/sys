a29d28de9c43118b99676ac469797198b8729dd0
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-05-30 11:57:30 +0900

    windows

--- resources/views/sys/references.blade.php
+++ resources/views/sys/references.blade.php
@@ -13,39 +13,50 @@
     <br>
 
     <div style="margin:0 auto; width: 50%">
-    <h2 style="text-align:center;">参考サイト</h2>
-    <br>
+      <h2 style="text-align:center;">参考サイト</h2>
 
-	  <h4>学習支援</h4>
-	  <a href="https://www.crammedia.com/">CramMedia</a>
-	  <br><br>
+	    <div class="card-header">
+        学習支援
+      </div>
+      <div class="card-body">
+        <a href="https://www.crammedia.com/">CramMedia</a>
+        <br><br>
 
-	  <a href="https://ping-t.com/">Ping-t</a>
-	  <br><br><br>
+        <a href="https://ping-t.com/">Ping-t</a>
+        <br><br><br>
+      </div>
+      <br>
 
+      <div class="card-header">
+        行政
+      </div>
+      <div class="card-body">
+        <a href="https://www.mext.go.jp/a_menu/shotou/zyouhou/detail/1403162.htm">小学校プログラミング教育の手引</a>
+        <br><br>
 
-	  <h4>行政</h4>
-          <a href="https://www.mext.go.jp/a_menu/shotou/zyouhou/detail/1403162.htm">小学校プログラミング教育の手引</a>
-	  <br><br>
+        <a href="https://www.meti.go.jp/policy/it_policy/jinzai/sugomori/index.html">巣ごもりDXステップ講座情報ナビ</a>
+        <br><br>
 
-          <a href="https://www.meti.go.jp/policy/it_policy/jinzai/sugomori/index.html">巣ごもりDXステップ講座情報ナビ</a>
-	  <br><br>
+        <a href="https://www.dnc.ac.jp/kyotsu/shiken_jouhou/r7ikou.html">独立行政法人　大学入試センター　令和７年度以降の試験</a>
+        <br><br>
 
-          <a href="https://www.dnc.ac.jp/kyotsu/shiken_jouhou/r7ikou.html">独立行政法人　大学入試センター　令和７年度以降の試験</a>
-	  <br><br>
+        <a href="https://www.meti.go.jp/policy/digital_transformation/index.html">経済産業省のデジタル・トランスフォーメーション</a>
+        <br><br>
 
-    <a href="https://www.meti.go.jp/policy/digital_transformation/index.html">経済産業省のデジタル・トランスフォーメーション</a>
-	  <br><br>
+        <a href="https://www.ipa.go.jp/">IPA 独立行政法人 情報処理推進機構</a>
+        <br><br>
+      </div>
 
-    <a href="https://www.ipa.go.jp/">IPA 独立行政法人 情報処理推進機構</a>
-	  <br><br>
+      <div class="card-header">
+        未分類
+      </div>
+      <div class="card-body">
+        <a href="https://nadesi.com/top/">日本語プログラミング言語「なでしこ」</a>
+        <br><br>
 
-	  <h4>未分類</h4>
-    <a href="https://nadesi.com/top/">日本語プログラミング言語「なでしこ」</a>
-    <br><br>
-
-    <a href="https://ja.softether.org/">SoftEther VPN プロジェクト</a>
-    <br><br><br>
+        <a href="https://ja.softether.org/">SoftEther VPN プロジェクト</a>
+        <br><br><br>
+      </div>
     </div>
 
     @endsection
