770adcdaa27ac41c99c3919f554a237e97464dc6
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-06-01 02:34:29 +0900

    windows

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,23 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->bigIncrements('id');                                // ID
+            $table->string('name');                                     // 氏名
+            $table->string('password');                                 // パスワード
+            $table->rememberToken();                                    // トークン
+            $table->string('email')->unique();                          // Eメールアドレス
+            $table->timestamp('email_verified_at')->nullable();         // Eメール認証日時
+
+            $table->integer('age')->nullable();                          // 年齢
+            $table->string('street_address')->nullable();               // 住所
+            $table->string('profession')->nullable();                   // 職業
+
+            $table->integer('ticket1hour')->default(0);                 // ５５分チケット
+            $table->integer('ticket15min')->default(0);                 // １５分チケット
+            $table->integer('ticket15min_trial')->default(2);           // １５分お試しチケット
+            $table->string('ip_address')->nullable();                   // ＩＰアドレス
+            $table->string('user_agent', 300)->nullable();              // ユーザーエージェント
+            $table->timestamps();                                       // タイムスタンプ
         });
     }
 
