3a8ecc65435467398b72a7b72034dbe1e073aa09
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-06-11 10:05:52 +0900

    windows

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -284,7 +284,6 @@ class ReservationController extends Controller
         }
 
 
-
         //dd($reservations);
 
         // 今月のデータしか取得出来ていない。前月もしくは翌月のデータが必要となるケースも存在する。
@@ -387,38 +386,6 @@ class ReservationController extends Controller
                             'flag_lastWeek'         =>  $flag_lastWeek,             // 「前週へ」ボタン表示フラグ
                             'flag_nextWeek'         =>  $flag_nextWeek,             // 「翌週へ」ボタン表示フラグ
                         ]);
-
-        return view('reservation.weekly_hour')
-                            ->with([
-                                'days'                  =>  $array_this_week_days,      // 該当週の日付配列
-                                'hours'                 =>  $array_hours,               // 配列 時間帯
-                                'minutes'               =>  $array_minutes,             // 配列 分
-                                'reservations'          =>  $reservations,              // 予約
-
-                                'year'                  =>  $work_year,                 // 該当日 年
-                                'month'                 =>  $work_month,                // 該当日 月
-                                'today'                 =>  $work_day,                  // 該当日 日(カーボン使用で日付が変動している)
-                                'numOfWeek'             =>  $numOfWeek,                 // 該当日 月内の第何週か    
-
-                                'now_year'              =>  $now_year,                  // 現在年
-                                'now_month'             =>  $now_month,                 // 現在月
-                                'now_day'               =>  $now_day,                   // 現在日付
-                                'now_hour'              =>  $now_hour,                  // 現在時間
-                                'now_minute'            =>  $now_minute,                // 現在分
-
-                                'last_week_ymd'         =>  $last_week_ymd,             // 先週末日の年月日
-                                'next_week_ymd'         =>  $next_week_ymd,             // 翌週初日の年月日
-
-                                'numOfDaysElapsed'      =>  $numOfDaysElapsed,          // 経過日数
-                                'numOfThisWeekDays'     =>  $numOfThisWeekDays,         // 該当週の当月日数
-                                'numOfNextWeekDays'     =>  $numOfNextWeekDays,         // 該当週の翌月日数
-                                'numOfLastMonthDays'    =>  $numOfLastMonthDays,        // 該当週の前月日数
-                                'user_reservations'     =>  $user_reservations,         // ユーザー予約情報
-
-                                'ticket'                =>  $ticket,                    // チケット情報
-                                'flag_lastWeek'         =>  $flag_lastWeek,             // 「前週へ」ボタン表示フラグ
-                                'flag_nextWeek'         =>  $flag_nextWeek,             // 「翌週へ」ボタン表示フラグ
-                            ]);
 
     }
 
