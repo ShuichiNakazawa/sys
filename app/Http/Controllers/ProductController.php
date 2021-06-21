b87389d9b19f93cb50892a9c288fd20d44b6ee80
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-06-21 09:57:54 +0900

    windows

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,4 +7,9 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     //
+    public function index()
+    {
+        return view('product.index')
+                ->with('products', Product::get());
+    }
 }
