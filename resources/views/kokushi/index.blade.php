b025efbe166503e53081090797605f93a04567cd
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-06-29 22:09:21 +0900

    windows

--- resources/views/kokushi/index.blade.php
+++ resources/views/kokushi/index.blade.php
@@ -42,6 +42,7 @@
                 <input type="hidden" name="subject_id" value="{{ $id }}">
                 <input type="hidden" name="subject_id" value="{{ $id }}">
               </form>
+              <br><br>
             {{--
             </a>
             --}}
