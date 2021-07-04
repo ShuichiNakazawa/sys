57fdedd7d8919410d86882f4fc7e1e7873cf7403
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-07-04 16:43:46 +0900

    windows

--- resources/views/sample/equip/index.blade.php
+++ resources/views/sample/equip/index.blade.php
@@ -27,16 +27,20 @@
           <br><br><br>
 
           {{-- マスタ管理権限が必要 --}}
-          <a href="{{ route('sample.equip.register_m_dept') }}">
-            <button>部門マスタ登録</button>
-          </a>
-          <br><br>
+          @if(Auth::user()->privilege_access < 3)
+            <a href="{{ route('sample.equip.register_m_dept') }}">
+              <button>部門マスタ登録</button>
+            </a>
+            <br><br>
+          @endif
 
-          {{-- 備品マスタ登録 --}}
-          <a href="{{ route('sample.equip.register_m_equip') }}">
-            <button>備品マスタ登録</button>
-          </a>
-          <br><br>
+          @if(Auth::user()->privilege_access < 3)
+            {{-- 備品マスタ登録 --}}
+            <a href="{{ route('sample.equip.register_m_equip') }}">
+              <button>備品マスタ登録</button>
+            </a>
+            <br><br>
+          @endif
 
           {{-- 権限を持ったユーザのみに表示 --}}
           @if(Auth::user()->privilege_access < 3)
