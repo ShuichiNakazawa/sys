a7fc25a5010455435763820ef826255f84be27d6
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-07-11 08:13:39 +0900

    windows

--- resources/views/kokushi/audio.blade.php
+++ resources/views/kokushi/audio.blade.php
@@ -15,13 +15,14 @@
   <div class="card-body">
     <h3>国試過去問</h3>
   </div>
+  看護師
   <div class="card-body">
     <div class="card-body">
       <figure>
-	<figcaption>令和２年 午前</figcaption>
-	<audio controls src="{{ asset('audio/ns/r2_bn.mp3') }}" controlslist="nodownload" oncontextmenu="return false;">
-	  <code>audio</code>
-	</audio>
+        <figcaption>令和２年 午前</figcaption>
+        <audio controls src="{{ asset('audio/ns/r2_bn.mp3') }}" controlslist="nodownload" oncontextmenu="return false;">
+          <code>audio</code>
+        </audio>
       </figure>
     </div>
 
