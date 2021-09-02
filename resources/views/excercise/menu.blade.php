e8725d7d8c2d7209746d4d0478f984ed48571d7a
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-09-02 19:43:30 +0900

    windows

--- resources/views/excercise/menu.blade.php
+++ resources/views/excercise/menu.blade.php
@@ -750,9 +750,9 @@
                         indexPlus1 = index + 1;
 
                         arrayResult[index] = [
-                            'questionNumber'    =  indexPlus1, 
-                            'questionSentence'  =  this.arrayQuestionSentences[index]['question_sentence'],
-                            'correct'           =  ""
+                            indexPlus1, 
+                            this.arrayQuestionSentences[index]['question_sentence'],
+                            ""
                         ];
 
                     }
