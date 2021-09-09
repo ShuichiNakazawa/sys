713e17dc606c5c3a7066002821f9872fd6bd20b9
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-09-09 22:46:07 +0900

    windows

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,8 +12,8 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-                            <span style="font-weight: bold;">※必須</span>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}<br>※必須</label>
+
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
