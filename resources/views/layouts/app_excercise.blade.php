55a9860795db49e4b8f08bce7c96b5c4c8bead09
ShuichiNakazawa <shu.nakazaw@gmail.com>
2021-09-09 18:16:58 +0900

    windows

--- resources/views/layouts/app_excercise.blade.php
+++ resources/views/layouts/app_excercise.blade.php
@@ -80,7 +80,14 @@
               </li>
             @else
               <li class="nav-item">
-                <a class="nav-link_custom" href="{{ url('') }}">ログアウト</a>
+                <a class="nav-link_custom" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
               </li>
             @endif
 
